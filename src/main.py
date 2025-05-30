96895c78eb2ade61c66b529e04478fcacd9d8548
Marco-gh <marcosilveri32@gmail.com>
2025-05-30 12:00:35 +0200

    Miglioramento modularità radar

--- src/main.py
+++ src/main.py
@@ -6,17 +6,22 @@ def main():
     dir_path = "/home/marco/Documents/TAV project/dataset/man-truckscenes/"
     trucksc = TruckScenes('v1.0-mini', dir_path, verbose=True)
 
-    first_sample_token = trucksc.scene[0]['first_sample_token']
+    first_sample_token = trucksc.scene[7]['first_sample_token']
 
-    X_test, X_train, y_test, y_train, y_pred, ex_time, labels = rd.radar_scene_analysis(trucksc, first_sample_token, dir_path)
-    # Matrice di confusione
+    # Parametro per numero di frame da usare in fase di test
+    test_size = 0.95
+    # Allenamento classificatore:
+    clf = rd.train_radar_classifier(trucksc, first_sample_token, dir_path, test_size)
+    # Uso classificatore:
+    X_test, y_test, y_pred, ex_time, labels = rd.radar_scene_analysis(trucksc, first_sample_token, dir_path, clf, test_size)
+
     cm = confusion_matrix(y_test, y_pred, labels=labels)
-    print(f"Matrice di confuzione:\n {cm}")
-    # Accuratezza: percentuale di predizioni corrette
+    print("Matrice di confusione:\n", cm)
+
     accuracy = accuracy_score(y_test, y_pred)
     print(f"Accuratezza: {accuracy:.2f}")
-    # Tempo di esecuzione
-    print(f"Tempo analisi dati radar: {ex_time}")
+
+    print(f"Tempo analisi dati radar: {ex_time:.1f} s")
 
 if __name__ == "__main__":
     main()
